6cb063aac9c198c81ffa30e13deea520f07ae44c
Hichxm <hicham.slimani.fr@gmail.com>
2025-05-30 01:29:18 +0200

    Add content wrapper to Layout component
    Introduced a `.content` wrapper in the Layout component's JSX and corresponding styles in `Layout.scss` to improve structural separation. This change allows better content organization and consistent styling for wrapped elements.

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,10 +1,13 @@
 import Header from "./Header.jsx";
 import './Layout.scss'
 
-export default function Layout() {
+export default function Layout({children}) {
     return <>
         <div className="layout">
             <Header />
+            <div className="content">
+                {children}
+            </div>
         </div>
     </>
 }
