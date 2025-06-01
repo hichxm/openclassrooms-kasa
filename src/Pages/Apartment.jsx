8d4371e08df05955a8582dd7c304b724976ab3f1
Hichxm <hicham.slimani.fr@gmail.com>
2025-06-01 14:07:40 +0200

    Add layout and styling for Apartment page
    Introduce a new layout with structured components and styles for the Apartment page, including content sections for tags, host profile, and ratings. Integrate `Tag` and `Stars` components for dynamic rendering based on the apartment data.

--- src/Pages/Apartment.jsx
+++ src/Pages/Apartment.jsx
@@ -4,6 +4,8 @@ import Layout from "../Components/Layout.jsx";
 import Carousel from "../Components/Carousel.jsx";
 
 import './Apartment.scss';
+import Tag from "../Components/Tag.jsx";
+import Stars from "../Components/Stars.jsx";
 
 
 export default function Apartment() {
@@ -11,5 +13,30 @@ export default function Apartment() {
 
     return <Layout>
         <Carousel images={apartment.pictures} />
+
+        <div className="apartment-content">
+            <div className="left-side">
+                <div className="content-header">
+                    <h1 className="title">{apartment.title}</h1>
+                    <span className="location">{apartment.location}</span>
+                </div>
+                {/* Tags */}
+                <div className="tags">
+                    {apartment.tags.map(tag => <Tag key={tag}>{tag}</Tag>)}
+                </div>
+            </div>
+            <div className="right-side">
+                <div className="profile">
+                    <div className="name">
+                        {apartment.host.name.split(' ').map((name, index) => <span key={name}>{name}{index === 0 ? <br /> : null}</span>)}
+                    </div>
+
+                    <img className="image" src={apartment.host.picture} alt=""/>
+                </div>
+                <div className="stars">
+                    <Stars totalStars={5} stars={apartment.rating} />
+                </div>
+            </div>
+        </div>
     </Layout>
 }
