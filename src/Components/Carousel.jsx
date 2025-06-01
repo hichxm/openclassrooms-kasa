039ea65e08bb315475de0a798319c3a84475cff2
Hichxm <hicham.slimani.fr@gmail.com>
2025-06-01 13:25:44 +0200

    Add image carousel to Apartment page
    Introduced a reusable `Carousel` component for displaying images, complete with navigation arrows and a slide counter. Integrated the carousel into the Apartment page to showcase apartment pictures dynamically. Added necessary assets and styling for the carousel functionality.

---
+++ src/Components/Carousel.jsx
@@ -0,0 +1,72 @@
+import LeftArrow from './../assets/arrow-left.svg'
+import RightArrow from './../assets/arrow-right.svg'
+
+import './Carousel.scss'
+import {useEffect, useRef, useState} from "react";
+
+export default function Carousel({images = []}) {
+    const slidesRef = useRef(null);
+    const [currentIndex, setCurrentIndex] = useState(-1);
+
+    useEffect(() => {
+        goToSlide(currentIndex);
+    }, [currentIndex])
+
+    const nextSlide = (event) => {
+        event.preventDefault();
+
+        let index = currentIndex;
+
+        if(index >= images.length - 2) {
+            index = -1;
+        } else {
+            index = index + 1;
+        }
+
+        setCurrentIndex(index)
+    }
+
+    const prevSlide = (event) => {
+        event.preventDefault();
+
+        let index = currentIndex;
+
+        if(currentIndex <= 0) {
+            index = images.length - 2;
+        } else {
+            index = index - 1;
+        }
+
+        setCurrentIndex(index);
+    }
+
+    const goToSlide = (slide) => {
+        slidesRef.current.style.transform = `translateX(-${(slide + 1) * 100}%)`;
+    }
+
+    return <div className="carousel">
+        {/* Carousel */}
+        <div className="slides" ref={slidesRef}>
+            {images.map((image, index) => <img
+                key={image}
+                src={image}
+                style={{ '--slide': index }}
+                className="slide" alt=""/>)}
+        </div>
+
+        {/* Arrow */}
+        <div className="arrows">
+            <a href="#" onClick={prevSlide} className="left-arrow">
+                <img src={LeftArrow} alt=""/>
+            </a>
+
+            <a href="#" onClick={nextSlide} className="right-arrow">
+                <img src={RightArrow} alt=""/>
+            </a>
+        </div>
+
+        <div className="counter">
+            <span>{currentIndex + 2}/{images.length}</span>
+        </div>
+    </div>
+}
--- src/Pages/Apartment.jsx
+++ src/Pages/Apartment.jsx
@@ -1,12 +1,15 @@
 import {useLoaderData} from "react-router";
 
 import Layout from "../Components/Layout.jsx";
+import Carousel from "../Components/Carousel.jsx";
+
 import './Apartment.scss';
+
 
 export default function Apartment() {
     const {apartment} = useLoaderData();
 
     return <Layout>
-        {apartment.title}
+        <Carousel images={apartment.pictures} />
     </Layout>
 }
