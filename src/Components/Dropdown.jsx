83b1d4d89c058464c3c3725d62f808d7de5c7716
Hichxm <hicham.slimani.fr@gmail.com>
2025-06-01 14:36:35 +0200

    Refactor Dropdown click handler for clarity and reuse.
    Replaced inline click handler with a separate `handleClick` function to improve readability and reusability. Also updated the default title prop from 'DROPDOWN_NAME' to 'DROPDOWN_TITLE' for consistency.

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -2,11 +2,17 @@ import Close from './../assets/close.svg';
 import './Dropdown.scss';
 import {useState} from "react";
 
-export default function Dropdown({children, title = 'DROPDOWN_NAME'}) {
+export default function Dropdown({children, title = 'DROPDOWN_TITLE'}) {
     const [isOpen, setIsOpen] = useState(false);
 
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        setIsOpen(!isOpen);
+    }
+
     return <div className="dropdown">
-        <a href="#" onClick={() => setIsOpen(!isOpen)} className="header">
+        <a href="#" onClick={handleClick} className="header">
             <span>{title}</span>
             <span>
                 <img src={Close} alt="Fermer"/>
